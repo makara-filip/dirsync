3b6d2f1cd52341972bc71f8a7878e50cbeb9a432
Filip Makara <filip.makara639@student.cuni.cz>
2025-04-18 16:06:23 +0200

    Fix mono-directional directory iteration

--- src/synchronize.cpp
+++ src/synchronize.cpp
@@ -3,7 +3,6 @@
 
 #include <filesystem>
 #include <iostream>
-// #include <ranges>
 #include <set>
 #include <string>
 
@@ -55,16 +54,15 @@ std::string insert_timestamp_to_filename(const fs::directory_entry &entry) {
 		+ entry.path().extension().string();
 }
 
+// TODO: rename to MonodirectionalContext ?
 struct RecursiveSynchronizationContext {
 	std::vector<std::optional<DirectoryConfiguration>>
 		source_configuration_stack, target_configuration_stack;
-	const std::filesystem::directory_entry *source_directory, *target_directory;
+	const std::filesystem::directory_entry *root_source, *root_target;
 
-	const std::chrono::system_clock::time_point time = std::chrono::system_clock::now();
-
-	RecursiveSynchronizationContext(fs::directory_entry *source, fs::directory_entry *target) {
-		this->source_directory = source;
-		this->target_directory = target;
+	RecursiveSynchronizationContext(const fs::directory_entry *source, const fs::directory_entry *target) {
+		this->root_source = source;
+		this->root_target = target;
 	}
 
 	bool should_copy(const fs::directory_entry &entry, const fs::file_status &status) const {
@@ -107,13 +105,17 @@ struct BidirectionalContext {
 	}
 };
 
-void delete_extra_target_entries(const ProgramArguments &arguments, const RecursiveSynchronizationContext &context) {
-	for (const fs::directory_entry &target_entry : fs::directory_iterator(*context.target_directory)) {
+void delete_extra_target_entries(
+	const ProgramArguments &arguments,
+	const fs::directory_entry &source_directory,
+	const fs::directory_entry &target_directory
+) {
+	for (const fs::directory_entry &target_entry : fs::directory_iterator(target_directory)) {
 		std::error_code err;
 		const fs::file_status status = fs::status(target_entry, err);
 
 		const fs::directory_entry matching_source_entry(
-			context.source_directory->path() / target_entry.path().filename(),
+			source_directory.path() / target_entry.path().filename(),
 			err
 		);
 		const fs::file_status source_status = fs::status(matching_source_entry, err);
@@ -221,7 +223,12 @@ final:
 	fs::copy_file(source_file, target_path, options, err);
 }
 
-int synchronize_directories_recursively(RecursiveSynchronizationContext context, const ProgramArguments &arguments) {
+int synchronize_directories_recursively(
+	const ProgramArguments &arguments,
+	RecursiveSynchronizationContext &context,
+	const fs::directory_entry &source_directory,
+	const fs::directory_entry &target_directory
+) {
 	// read this directory configurations, both source and target
 	// add them to the configuration stack
 	// make a composite configuration? - compressing all what to copy and what not
@@ -230,15 +237,16 @@ int synchronize_directories_recursively(RecursiveSynchronizationContext context,
 	// - if a directory, call itself recursively with deeper configuration stack
 
 	std::optional<DirectoryConfiguration> source_configuration, target_configuration;
-	int error = get_directory_configuration(*context.source_directory, arguments, source_configuration);
-	error |= get_directory_configuration(*context.target_directory, arguments, target_configuration);
+
+	int error = get_directory_configuration(source_directory, arguments, source_configuration);
+	error |= get_directory_configuration(target_directory, arguments, target_configuration);
 
 	context.source_configuration_stack.push_back(source_configuration);
 	context.target_configuration_stack.push_back(target_configuration);
 
 	if (error) return error;
 
-	for (const fs::directory_entry &source_entry : fs::directory_iterator(*context.source_directory)) {
+	for (const fs::directory_entry &source_entry : fs::directory_iterator(source_directory)) {
 		std::error_code err;
 		const fs::file_status status = fs::status(source_entry, err);
 		if (err) {
@@ -247,23 +255,25 @@ int synchronize_directories_recursively(RecursiveSynchronizationContext context,
 		}
 
 		const fs::directory_entry matching_target_entry(
-			context.target_directory->path() / source_entry.path().filename(),
+			target_directory.path() / source_entry.path().filename(),
 			err
 		);
 
 		if (fs::is_directory(status)) {
 			if (!context.should_copy(source_entry, status)) continue;
-			RecursiveSynchronizationContext recursive_context = context;
-			recursive_context.source_directory = &source_entry;
-			recursive_context.target_directory = &matching_target_entry;
-			error = synchronize_directories_recursively(recursive_context, arguments);
+			error = synchronize_directories_recursively(
+				arguments,
+				context,
+				source_entry,
+				matching_target_entry
+			);
 		} else if (fs::is_regular_file(status)) {
 			synchronize_file(arguments, context, source_entry, status, matching_target_entry, err);
 		}
 	}
 
 	if (arguments.delete_extra_target_files)
-		delete_extra_target_entries(arguments, context);
+		delete_extra_target_entries(arguments, source_directory, target_directory);
 
 	context.source_configuration_stack.pop_back();
 	context.target_configuration_stack.pop_back();
@@ -339,10 +349,17 @@ int synchronize_directories_bidirectionally(
 			}
 
 			// TODO: is there loss of configuration data?
+			// Possible solution?: convert from recursive one-directional context to bidirectional one
+			// while keeping the loaded directory configurations
 
 			if (source->is_directory()) {
 				RecursiveSynchronizationContext single_direction_context(&source->entry, &target->entry);
-				synchronize_directories_recursively(single_direction_context, arguments);
+				synchronize_directories_recursively(
+					arguments,
+					single_direction_context,
+					source->entry,
+					target->entry
+				);
 			} else if (fs::is_regular_file(source->status)) {
 				if (arguments.verbose) std::cout << "Copying " << source->entry << "\n";
 				if (arguments.dry_run) continue;
@@ -381,7 +398,7 @@ int synchronize_directories(const ProgramArguments &arguments) {
 		if (error) return error;
 
 		RecursiveSynchronizationContext context(&source_directory, &target_directory);
-		error = synchronize_directories_recursively(context, arguments);
+		error = synchronize_directories_recursively(arguments, context, source_directory, target_directory);
 	} else {
 		// we do not have source and target directories, we have two source ones
 
